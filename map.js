f23e0ab9c6b1f90fa51529ca8c9255c6e67fcf37
Jo LaBounty <jlabounty@pphs.purdue.edu>
2018-06-29 10:46:19 -0400

    first commit

---
+++ map.js
@@ -0,0 +1,28 @@
+const map = [
+    "WWWWWWWWWWWWWW",
+    "WWW   WW    WW",
+    "WOSB        WW",
+    "WWW BOW BO   W",
+    "WOWWB W   BO W",
+    "W W O WW   BOW",
+    "WB XBBO      W",
+    "W   O     WWWW",
+    "WWWWWWWWWWWWWW"
+];
+
+/* Alternate map
+
+const map = [
+   "    WWWWW          ",
+   "    W   W          ",
+   "    WB  W          ",
+   "  WWW  BWW         ",
+   "  W  B B W         ",
+   "WWW W WW W   WWWWWW",
+   "W   W WW WWWWW  OOW",
+   "W B  B          OOW",
+   "WWWWW WWW WSWW  OOW",
+   "    W     WWWWWWWWW",
+   "    WWWWWWW        "
+]
+ */
